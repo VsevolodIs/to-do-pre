d79225e214b29d1c48f35cdec88dc4e82a845a81
VsevolodIs <si34284@gmail.com>
2026-05-21 19:03:32 +0300

    cteate, save, delete and dublicate are made

--- index.js
+++ index.js
@@ -12,24 +12,78 @@ const formElement = document.querySelector(".to-do__form");
 const inputElement = document.querySelector(".to-do__input");
 
 function loadTasks() {
-
+    const savedTasks = localStorage.getItem("tasks");
+    if (savedTasks) {
+        return JSON.parse(savedTasks);
+    }
+    return items
 }
 
 function createItem(item) {
-	const template = document.getElementById("to-do__item-template");
-	const clone = template.content.querySelector(".to-do__item").cloneNode(true);
-  const textElement = clone.querySelector(".to-do__item-text");
-  const deleteButton = clone.querySelector(".to-do__item-button_type_delete");
-  const duplicateButton = clone.querySelector(".to-do__item-button_type_duplicate");
-  const editButton = clone.querySelector(".to-do__item-button_type_edit");
+    const template = document.getElementById("to-do__item-template");
+    const clone = template.content.querySelector(".to-do__item").cloneNode(true);
+    const textElement = clone.querySelector(".to-do__item-text");
+    const deleteButton = clone.querySelector(".to-do__item-button_type_delete");
+    const duplicateButton = clone.querySelector(".to-do__item-button_type_duplicate");
+    const editButton = clone.querySelector(".to-do__item-button_type_edit");
 
+    textElement.textContent = item;
+
+    deleteButton.addEventListener("click", function () {
+        clone.remove();
+        items = getTasksFromDOM();
+        saveTasks(items);
+    });
+
+    duplicateButton.addEventListener("click", function () {
+        const itemName = textElement.textContent;
+        const newItem = createItem(itemName);
+        listElement.prepend(newItem);
+        items = getTasksFromDOM();
+        saveTasks(items);
+    });
+
+    return clone;
 }
 
-function getTasksFromDOM() {
+items = loadTasks();
+items.forEach(function (item) {
+    listElement.append(createItem(item));
+});
 
+function getTasksFromDOM() {
+    const itemsNamesElements = document.querySelectorAll(".to-do__item-text");
+    const tasks = [];
+    itemsNamesElements.forEach(function (element) {
+        tasks.push(element.textContent);
+    });
+    return tasks;
 }
 
 function saveTasks(tasks) {
-
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 }
+
+items = loadTasks();
+
+items.forEach(function (item) {
+    const element = createItem(item);
+    listElement.append(element);
+});
+
+saveTasks(items);
+
+formElement.addEventListener("submit", function (evt) {
+    evt.preventDefault();
+
+    const taskText = inputElement.value;
+
+    const element = createItem(taskText);
+    listElement.prepend(element);
+
+    items = getTasksFromDOM();
+    saveTasks(items);
+
+    inputElement.value = "";
+});
 
